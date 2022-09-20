4d8062212945baf079134a13f71e4178a83c59e4
Harita Parikh <88765812+20it079@users.noreply.github.com>
2022-09-20 12:17:19 +0530

    Update PatientRecordController.java

--- src/main/java/com/example/demo/controller/PatientRecordController.java
+++ src/main/java/com/example/demo/controller/PatientRecordController.java
@@ -1,5 +1,6 @@
 package com.example.demo.controller;
 
+
 import com.example.demo.model.PatientRecord;
 import com.example.demo.repositories.PatientRecordRepository;
 import org.springframework.beans.factory.annotation.Autowired;
