3885f956d7523e5edec6f2ec65e70435e4d54f9b
Harita Parikh <88765812+20it079@users.noreply.github.com>
2022-09-20 12:19:14 +0530

    Update PatientRecordRepository.java

--- src/main/java/com/example/demo/repositories/PatientRecordRepository.java
+++ src/main/java/com/example/demo/repositories/PatientRecordRepository.java
@@ -1,6 +1,7 @@
 package com.example.demo.repositories;
 
 import com.example.demo.model.PatientRecord;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PatientRecordRepository extends JpaRepository<PatientRecord, Long> {
